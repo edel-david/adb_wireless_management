dba03c5c2ce32666fe5005fdd5f381af118bcb76
David Edel <david.edel@gmx.de>
2023-07-05 17:39:31 +0200

    added pair and disconnect

--- wireless_adb.py
+++ wireless_adb.py
@@ -8,8 +8,8 @@ from sys import platform, argv
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-logger=logging.getLogger("ADBW")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("ADBW")
 print(logger)
 
 logger.debug("wireless_adb.py logger is active")
@@ -25,15 +25,12 @@ logger.debug("wireless_adb.py logger is active")
 #     def __init__(self, env_var_name):
 #         raise KeyError(f"{env_var_name} was not found in .env file or ENV Vars")
 
+class IPNotFoundError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
 
 PLATFORM = platform
-
 load_dotenv()
-
-# class Buffer():
-#     ip:None | str
-#     def __init__(self):
-#         self.ip=None
 
 
 SCRCPY_PATH = os.getenv("SCRCPY_PATH")
@@ -41,11 +38,6 @@ SCRCPY_PATH = os.getenv("SCRCPY_PATH")
 DEFAULT_PHONE_IP = os.getenv("DEFAULT_PHONE_IP")
 
 # ADB GLOBAL OPTIONS -d will use the usb device and -e will use the TCP/IP device (wifi device) or an emulator (why wifi of emulator?)
-
-
-class IPNotFoundError(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
 
 
 def check_if_ip(possible_ip: str) -> bool:
@@ -87,7 +79,10 @@ def connect_to_wireless(ip: str, port: str | int = "5555"):
     port = str(port)
     if ip is None:
         return
-    ret = subprocess.run(["adb", "connect", f"{ip}:{port}"], capture_output=True,)
+    ret = subprocess.run(
+        ["adb", "connect", f"{ip}:{port}"],
+        capture_output=True,
+    )
     print(f"{ret.stdout.decode().strip()} {ret.stderr.decode().strip()}")
 
 
@@ -103,10 +98,13 @@ def stop_tcp_server():
 
 def start_debugging_server():
     try:
-        ret = subprocess.run("adb -d tcpip 5555".split(" "), capture_output=True,)
+        ret = subprocess.run(
+            "adb -d tcpip 5555".split(" "),
+            capture_output=True,
+        )
     except subprocess.CalledProcessError as e:
-        print("COMMAND FAILED, maybe try pairing?. Error:")
-    finally:    
+        print(F"COMMAND FAILED, maybe try pairing?. Error: {e}")
+    finally:
         print(f"{ret.stdout.decode().strip()} {ret.stderr.decode().strip()}")
 
 
@@ -118,10 +116,12 @@ def turn_on_wlan():
     connect_to_wireless(phone_ip)
 
 
-def list_devices():
-    ret = subprocess.run(
-        ["adb", "devices", "-l"], capture_output=True
-    )  # the -l will show the devices' product name, model name and device name (and transport_id?)
+def list_devices(*args):
+    adb_devices_args = ["adb", "devices"]
+    for param in args:
+        adb_devices_args.append(param)
+    ret = subprocess.run(args=adb_devices_args, capture_output=True)
+    # the -l will show the devices' product name, model name and device name (and transport_id?)
     print(ret.stdout.decode())
 
 
@@ -133,7 +133,6 @@ def launch_scrcpy(*scrcpy_args):
 
 
 def launch_scrcpy_thread(con: Connection, port=5555, *scrcpy_args):
-
     ip = get_adb_android_ip("-e")  # TODO add provided arg instead of -e
     if SCRCPY_PATH is None:
         print("no SCRCPY_PATH env var found")
@@ -202,8 +201,40 @@ def connect_wireless_random_port(ip_poss_port: str | None):
     connect_to_wireless(ip, port)
     print("Connected?")
 
+def pair(*args):
+    # *args could be (192.168.23.23,38980,745278), or (40000,987899)
+    ip = DEFAULT_PHONE_IP
+    port = 0
+    code = 0
+    if len(args) == 3:
+        ip = args[0]
+        port = args[1]
+        code = args[2]
 
-def main(init_input:list[str]|None=None):
+    elif len(args) == 2:
+        port = args[0]
+        code = args[1]
+
+    else:
+        print("wrong amount of arguments to pair.\nTry the following format(192.168.23.23(ip, can be omitted if env var set),38980:port,745278:pairing code)")
+        return
+
+    if ip == None:
+        print("no ip found (amount of args !=3 and no env var DEFAULT_PHONE_IP found)")   
+        return
+
+
+    ret = subprocess.run(
+        args=[
+            "adb",
+            "pair",
+            ip,
+            port,
+            code
+        ]
+    )
+
+def main(init_input: list[str] | None = None):
     global port
 
     commands_description = {
@@ -211,28 +242,44 @@ def main(init_input:list[str]|None=None):
         "wlan": "alias for wifi",
         "stop": '"usb" or "stop" will stop the wireless debugging daemon on the phone if it was started from usb ("adb tcpip 5555" or "adbw wifi"). Unfortunately it is not easily possible to stop the daemon if started with developer quick tile or in developer options. adb usb will restart wireless debugging with a different port. (Please tell me if thats wrong!',
         "usb": "alias for stop",
-        "help" : "shows help message. can show only one help by specifying the command: 'help usb'",
+        "help": "shows help message. can show only one help by specifying the command: 'help usb'",
         "devices": "used 'adb devices -l' to show you all known devices",
         "status": "alias for devices",
         "power": "simulates a press of the power button",
-        "connect": "to connect to adb wireless if you used the android 11+ quick settings developer tile to enable wireless debuging. This trash tile uses an random port, which is why we scan all possible ports with nmap (must be installed) You can pass an ip:port pair (or soon just the port)" , #TODO pass just port and append to default phone ip
+        "connect": "to connect to adb wireless if you used the android 11+ quick settings developer tile to enable wireless debuging. This trash tile uses an random port, which is why we scan all possible ports with nmap (must be installed) You can pass an ip:port pair (or soon just the port)",  # TODO pass just port and append to default phone ip
         "exit": "exits the programm",
     }
 
     try:
         p: Process
-        list_devices()
-        inp=init_input
+        # list_devices() # reomve forced adb devices at startup
+        inp = init_input
         while True:
-            
             match inp:
-                # TODO adb pair command
+                case ["status", *args] | [
+                    "devices",
+                    *args,
+                ]:
+                    # *args will be passed to adb devices (only -l)
+                    list_devices(*args)
+                case ["pair", *args]:
+                    print("trying to pair")
+                    pair(*args)
+                case ["disconnect",*args]:
+                    def disconnect(*args):
+                        adb_args=["adb","disconnect"]
+                        for arg in args:
+                            adb_args.append(arg)
+                        ret = subprocess.run(adb_args,capture_output=True)
+                        print(ret.stdout)
+                        print(ret.stderr)
+                    disconnect(*args)
+
                 case ["wlan"] | ["wifi"]:
                     turn_on_wlan()
                 case ["usb"] | ["stop"]:
                     stop_tcp_server()
-                case ["status"] | ["devices"]:
-                    list_devices()
+
                 case ["scrcpy", *scrcpy_args]:
                     print(f" scrcpy args are: {scrcpy_args}")
                     launch_scrcpy(*scrcpy_args)
@@ -250,39 +297,42 @@ def main(init_input:list[str]|None=None):
                         connect_wireless_random_port(ip_poss_port[0])
                     else:
                         connect_wireless_random_port(None)
-                case ["pair"]:
-                    #pair mode
-                    pass
-                    
+
                 case ["help"]:
                     print("Commands: ")
-                    toprint=commands_description.items()
-                    toprint=[F"{command:10.10}:  {description}" for command, description in toprint]
+                    toprint = commands_description.items()
+                    toprint = [
+                        f"{command:10.10}:  {description}"
+                        for command, description in toprint
+                    ]
                     print("\n".join(toprint))
                 case [
                     "help",
-                    *command,]:  # case where you want help for a specific command
-                    print("todo!")  # TODO implement 
+                    *command,
+                ]:  # case where you want help for a specific command
+                    print("todo!")  # TODO implement
                 case ["exit"]:
                     break
                 case None:
-                    pass
                     # this is init
-                
+                    pass
+
                 case _:
                     print("could not identify command")
             inp = input(
-                "wifi | usb | status | scrcpy | power | connect(random port) | exit | help \n>>> ")  
-            inp=inp.split(" ")
+                "wifi | usb | status | scrcpy | power | connect(random port) | exit | help \n>>> "
+            )
+            inp = inp.split(" ")
     except KeyboardInterrupt:
         pass
     print("end")
-    
 
 
 if __name__ == "__main__":
-    sysargs_string=argv[1:] # the first argument will be the file path
-    sysargs_passed=sysargs_string if sysargs_string else None  # will be None if the list is empty, else the list
-    logger.info(F"{sysargs_passed=}")
+    sysargs_string = argv[1:]  # the first argument will be the file path
+    sysargs_passed = (
+        sysargs_string if sysargs_string else None
+    )  # will be None if the list is empty, else the list
+    logger.info(f"{sysargs_passed=}")
     port = 5555
     main(sysargs_passed)
